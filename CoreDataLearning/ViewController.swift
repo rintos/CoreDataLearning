d99479e95c2a6c7772f1e83494410290a7839158
VictorSantoryu <victor.santoryu@github.com>
2018-12-11 21:37:35 -0200

    Filtro ordenacaoAZ

--- CoreDataLearning/ViewController.swift
+++ CoreDataLearning/ViewController.swift
@@ -7,12 +7,67 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        /*
+        //agora preciso criar o produto
+        let produto = NSEntityDescription.insertNewObject(forEntityName: "Produto", into: context)
+        
+        //acesso os atributos
+        
+        produto.setValue("Rx580", forKey: "nome")
+        produto.setValue(300.28, forKey: "preco")
+        produto.setValue(1, forKey: "quantidade")
+        
+        do {
+            try context.save()
+            print("salvou dados com sucesso")
+        } catch  {
+            print("nao pode salvar")
+        }*/
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        // criar requisicao
+        let resultProduto = NSFetchRequest<NSFetchRequestResult>(entityName: "Produto")
+        
+        //Ordernar de A-Z
+        let ordenacaoAZ = NSSortDescriptor(key: "nome", ascending: false)
+        let ordenacaoZA = NSSortDescriptor(key: "preco", ascending: true)
+        
+        //aplicar filtro criados a requisicao
+        resultProduto.sortDescriptors = [ordenacaoAZ,ordenacaoZA]
+        
+        
+        do {
+            let produtos = try context.fetch(resultProduto)
+            
+            if produtos.count > 0 {
+                
+                for produto in produtos as! [NSManagedObject] {
+                    if let nome = produto.value(forKey: "nome") {
+                        if let quantidade = produto.value(forKey: "quantidade") {
+                            if let preco = produto.value(forKey: "preco") {
+                                
+                                print("o nome eh \(nome) a sua quantidade eh \(quantidade) o preco eh: \(preco)" )
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }catch  {
+            print(error)
+        }
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
